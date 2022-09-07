3654a56f8c899de43f5b9066d1cf8223914681b9
SANDI <sandi@SANDIs-MacBook-Pro.local>
2022-09-07 23:25:14 +0700

    add table

--- src/components/Pages/ProposalPages.js
+++ src/components/Pages/ProposalPages.js
@@ -1,17 +1,55 @@
 import React from "react";
-import { Col, Row, Descriptions, Skeleton } from "antd";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Col, Row, Descriptions, Skeleton, message, Table } from "antd";
 import MakeProposalForm from "../MakeProposalForm";
 
+const columns = [
+  {
+    title: 'Project',
+    dataIndex: 'project',
+    key: 'project',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+    key: 'status',
+  }
+];
+
 export default function ProposalPages() {
+  const [proposal, setProposal] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const getProposal = async () => {
+    axios.get('https://www.geo-circle.com/api/project/')
+    .then((res) => {
+      setProposal(res.data);
+      setLoading(false);
+    })
+    .catch(() => message.error("Something error with server"));
+  };
+
+  useEffect(() => {
+    getProposal();
+  }, []);
   return (
     <div className="proposal-page">
       <Row>
         <Col xs={24} xl={8}>
           <Descriptions title="User Info" layout="vertical">
-            <Descriptions.Item label="User Name">Zhou Maomao</Descriptions.Item>
-            <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+            <Descriptions.Item label="User Name">
+              Anugrah Sandi Rizqi
+            </Descriptions.Item>
+            <Descriptions.Item label="Telephone">
+              082170829587
+            </Descriptions.Item>
             <Descriptions.Item label="Live">
-              Hangzhou, Zhejiang
+              Jakarta, Indonesia
             </Descriptions.Item>
             <Descriptions.Item label="Address" span={2}>
               Kebon Kcang 5, Jakarta Indonesia
@@ -27,7 +65,11 @@ export default function ProposalPages() {
         </Col>
         <Col xs={24} xl={8}>
           <h4>List Proposal History :</h4>
-          <Skeleton />
+          {loading ? (
+            <Skeleton />
+          ) : (
+            <Table dataSource={proposal} columns={columns} />
+          )}
         </Col>
       </Row>
     </div>
